243f659b8da4f3dbf8d2edef78d0ca38f54fdeb0
anticode <146665183+anticode-dev@users.noreply.github.com>
2024-01-11 10:39:36 +0100

    feat: add emacs config link

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -34,25 +34,17 @@ export const code = async (args: string[]): Promise<string> => {
 
   return ` <a href="${url}" target="_blank">${url}</a>`;
 };
+export const emacs = async (args: string[]): Promise<string> => {
+  const url = `https://github.com/anticode-dev/emacs`;
+
+  return ` <a href="${url}" target="_blank">${url}</a>`;
+};
 
 
 export const whoami = async (args: string[]): Promise<string> => {
   return `${config.ps1_username}`;
 };
 
-export const emacs = async (args?: string[]): Promise<string> => {
-  return `so based`;
-};
 export const musings = async (args?: string[]): Promise<string> => {
   return `yea now how do I view publish it here hmmm`;
-};
-
-export const sudo = async (args?: string[]): Promise<string> => {
-  // window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank'); // ...I'm sorry
-  return `Permission denied: with little power comes... no responsibility? `;
-};
-
-// Banner
-export const banner = (args?: string[]): string => {
-  return;
 };
